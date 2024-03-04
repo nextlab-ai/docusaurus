e141c99e5592173468a8d268f1a398c320c0f336
ShinJaeIL01 <jay101@nextlab.co.kr>
2024-03-04 18:42:36 +0900

    config baseUrl 수정

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   url: "https://nextlab-ai.github.io/docusaurus",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/docusaurus/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
